074ddf75407ed5a22416fb33916d21473727de1e
David Vielhuber <david@vielhuber.de>
2018-05-22 00:55:45 +0200

    updated google anlaytics

--- wp-content/themes/blank/functions.php
+++ wp-content/themes/blank/functions.php
@@ -8,38 +8,6 @@ function is_production()
     return (strpos($_SERVER['HTTP_HOST'], '.local') === false && strpos($_SERVER['HTTP_HOST'], '192.168.178') === false);
 }
 
-/* google analytics */
-add_action('wp_head', function()
-{
-    $property = 'UA-12350231-1';
-    ?><script>
-        if(navigator.userAgent.indexOf('Speed Insights') === -1)
-        {
-            /* opt out */
-            var disableStr = 'ga-disable-<?php echo $property; ?>';
-            if( document.cookie.indexOf(disableStr + '=true') > -1 ) { window[disableStr] = true; }
-            document.addEventListener('DOMContentLoaded', function() { document.addEventListener('click', function(e) { if( e.target.tagName === 'A' && e.target.textContent === 'Google Analytics deaktivieren' ) { alert('Google Analytics wurde deaktiviert'); document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/'; window[disableStr] = true; e.preventDefault(); } }, true); });
-
-            document.addEventListener('DOMContentLoaded', function()
-            {
-                var script = null;
-
-                script = document.createElement('script');
-                script.src = 'https://www.googletagmanager.com/gtag/js?id=<?php echo $property; ?>';
-                document.head.appendChild(script);
-
-                script = document.createElement('script');
-                script.innerHTML += 'window.dataLayer = window.dataLayer || [];';
-                script.innerHTML += 'var gtag = function() { dataLayer.push(arguments); };';
-                script.innerHTML += 'gtag(\'js\', new Date());';
-                // track page view
-                script.innerHTML += 'gtag(\'config\', \'<?php echo $property; ?>\', { \'anonymize_ip\': true });';
-                document.head.appendChild(script);
-            });
-        }
-    </script><?php
-});
-
 // load js
 add_action('wp_enqueue_scripts', function()
 {
