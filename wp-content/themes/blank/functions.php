d2293d759aa21f488c04fc26eb216f93fb2a9891
David Vielhuber <david@vielhuber.de>
2018-06-29 00:08:05 +0200

    improved syntax

--- wp-content/themes/blank/functions.php
+++ wp-content/themes/blank/functions.php
@@ -15,27 +15,25 @@ if(
     }
     else
     {
-        echo '
-            <!DOCTYPE html>
-            <html lang="de">
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-                <meta name="robots" content="noindex" />
-                <title>'.get_bloginfo('name').'</title>
-                <style>
-                    * { box-sizing: border-box;margin:0;padding:0; }
-                    .form { position: absolute;top: 50%;transform: translateY(-50%);width:100%; }
-                    .form__input { border:2px solid #000;padding:20px;text-align:center;width:300px;font-size:30px;font-weight:bold;max-width:90%;margin:0 auto;display:block;outline:none; }
-                </style>
-            </head>
-            <body>
-                <form class="form" method="post" autocomplete="off">
-                    <input class="form__input" name="password" type="text" placeholder="" autofocus="autofocus" required="required" />
-                </form>
-            </body>
-            </html>
-        ';
+        echo '<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
+    <meta name="robots" content="noindex" />
+    <title>'.get_bloginfo('name').'</title>
+    <style>
+        * { box-sizing: border-box;margin:0;padding:0; }
+        .form { position: absolute;top: 50%;transform: translateY(-50%);width:100%; }
+        .form__input { border:2px solid #000;padding:20px;text-align:center;width:300px;font-size:30px;font-weight:bold;max-width:90%;margin:0 auto;display:block;outline:none; }
+    </style>
+</head>
+<body>
+    <form class="form" method="post" autocomplete="off">
+        <input class="form__input" name="password" type="text" placeholder="" autofocus="autofocus" required="required" />
+    </form>
+</body>
+</html>';
         die();
     }
 }
