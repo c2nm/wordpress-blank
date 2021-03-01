e11f44b50d7e01a56093634db3afbf7a3d5b23f6
David Vielhuber <david@vielhuber.de>
2021-03-01 10:58:57 +0100

    Add password protection support in template

--- wp-content/themes/blank/page.php
+++ wp-content/themes/blank/page.php
@@ -14,18 +14,25 @@ if( have_posts() )
 // acf style
 if( have_rows('blocks') )
 {
-    echo '<div class="items">';
-    while( have_rows('blocks') ) { the_row();        
-        if( file_exists( get_template_directory().'/_blocks/'.get_row_layout().'.php' ) )
-        {
-            echo '<div class="item item--'.get_row_layout().'">';
-                echo '<div class="item__inner">';
-                    include( get_template_directory().'/_blocks/'.get_row_layout().'.php' );
+    if (!post_password_required()) {
+        echo '<div class="items">';
+        while( have_rows('blocks') ) { the_row();        
+            if( file_exists( get_template_directory().'/_blocks/'.get_row_layout().'.php' ) )
+            {
+                echo '<div class="item item--'.get_row_layout().'">';
+                    echo '<div class="item__inner">';
+                        include( get_template_directory().'/_blocks/'.get_row_layout().'.php' );
+                    echo '</div>';
                 echo '</div>';
-            echo '</div>';
+            }
         }
+        echo '</div>';
     }
-    echo '</div>';
+    else {
+        echo '<div class="password-protection">';
+            echo get_the_password_form();
+        echo '<div>';
+    }
 }
 
 get_footer();
