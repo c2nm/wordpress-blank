6c978844a70a374349f224fdc1f7b385babd7749
David Vielhuber <david@vielhuber.de>
2022-04-28 12:43:56 +0200

    Migrate to classes.

--- wp-content/themes/blank/header.php
+++ wp-content/themes/blank/header.php
@@ -1,16 +1,22 @@
 <?php
-echo ascii_art();
+echo \WP\Core::asciiArt();
 echo '<!DOCTYPE html>';
-echo '<html '; language_attributes(); echo ' class="font-custom responsive-typography selection-color scrollbar">';
+echo '<html ';
+language_attributes();
+echo ' class="font-custom responsive-typography selection-color scrollbar' .
+    (!\WP\Core::isProduction() ? ' dev' : '') .
+    '">';
 echo '<head>';
-    echo '<meta charset="'.get_bloginfo('charset').'" />';
-    echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1" />';
-    wp_head();
+echo '<meta charset="' . get_bloginfo('charset') . '" />';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1" />';
+wp_head();
 echo '</head>';
-echo '<body '; body_class(); echo '>';
-    echo '<header>';
-        wp_nav_menu([
-            'theme_location' => 'main-menu'  
-        ]);
-    echo '</header>';
-    echo '<main>';
+echo '<body ';
+body_class();
+echo '>';
+echo '<header>';
+wp_nav_menu([
+    'theme_location' => 'main-menu',
+]);
+echo '</header>';
+echo '<main>';
