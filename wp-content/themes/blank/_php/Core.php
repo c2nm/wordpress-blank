9fce1d4c5963881bea2bce83e66a708225b8ed8f
David Vielhuber <david@vielhuber.de>
2022-07-25 23:09:43 +0200

    Added fix for pdf preview with black backgrounds.

--- wp-content/themes/blank/_php/Core.php
+++ wp-content/themes/blank/_php/Core.php
@@ -55,6 +55,7 @@ class Core
         $this->addBasicImageSizes();
         $this->preventResizeOfBigImages();
         $this->backendSearchInAltTags();
+        $this->fixPdfPreviewBlackBackgrounds();
         //$this->forceUseOfImageMagick();
         //$this->disableStripExifIptcFromImages();
         //$this->increaseImageQualityOfResizedImages();
@@ -461,6 +462,29 @@ $rand
             },
             0
         );
+    }
+
+    private function fixPdfPreviewBlackBackgrounds()
+    {
+        /* pdf preview black backgrounds bugfix (see https://core.trac.wordpress.org/ticket/45982) */
+        require_once ABSPATH . 'wp-includes/class-wp-image-editor.php';
+        require_once ABSPATH . 'wp-includes/class-wp-image-editor-imagick.php';
+        final class ExtendedWpImageEditorImagick extends WP_Image_Editor_Imagick {
+            protected function pdf_load_source() {
+                $loaded = parent::pdf_load_source();
+                try {
+                    $this->image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
+                    $this->image->setBackgroundColor('#ffffff');
+                } catch (Exception $exception) {
+                    error_log($exception->getMessage());
+                }
+                return $loaded;
+            }
+        }
+        add_filter('wp_image_editors', function (array $editors): array {
+            array_unshift($editors, ExtendedWpImageEditorImagick::class);
+            return $editors;
+        });
     }
 
     private function increaseImageQualityOfResizedImages()
