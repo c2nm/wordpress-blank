3be2725db18ae8d07b570c380ab510ae156a3d96
David Vielhuber <david@vielhuber.de>
2022-07-26 13:51:49 +0200

    Improve boilerplate.

--- wp-content/themes/blank/_php/Core.php
+++ wp-content/themes/blank/_php/Core.php
@@ -55,7 +55,6 @@ class Core
         $this->addBasicImageSizes();
         $this->preventResizeOfBigImages();
         $this->backendSearchInAltTags();
-        $this->fixPdfPreviewBlackBackgrounds();
         //$this->forceUseOfImageMagick();
         //$this->disableStripExifIptcFromImages();
         //$this->increaseImageQualityOfResizedImages();
@@ -348,11 +347,11 @@ $rand
     private function sendMailsNotOnProductionToDeveloper()
     {
         if (!self::isProduction()) {
-            // exclude welcome reset mails
-            if( strpos($data['subject'], 'Anmeldedaten') !== false ) {
-                return $data;
-            }
             add_filter('wp_mail', function ($data) {
+                // exclude welcome reset mails
+                if( strpos($data['subject'], 'Anmeldedaten') !== false ) {
+                    return $data;
+                }
                 $data['to'] =
                     isset($_SERVER['SERVER_ADMIN']) &&
                     $_SERVER['SERVER_ADMIN'] != '' &&
@@ -462,29 +461,6 @@ $rand
             },
             0
         );
-    }
-
-    private function fixPdfPreviewBlackBackgrounds()
-    {
-        /* pdf preview black backgrounds bugfix (see https://core.trac.wordpress.org/ticket/45982) */
-        require_once ABSPATH . 'wp-includes/class-wp-image-editor.php';
-        require_once ABSPATH . 'wp-includes/class-wp-image-editor-imagick.php';
-        final class ExtendedWpImageEditorImagick extends WP_Image_Editor_Imagick {
-            protected function pdf_load_source() {
-                $loaded = parent::pdf_load_source();
-                try {
-                    $this->image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
-                    $this->image->setBackgroundColor('#ffffff');
-                } catch (Exception $exception) {
-                    error_log($exception->getMessage());
-                }
-                return $loaded;
-            }
-        }
-        add_filter('wp_image_editors', function (array $editors): array {
-            array_unshift($editors, ExtendedWpImageEditorImagick::class);
-            return $editors;
-        });
     }
 
     private function increaseImageQualityOfResizedImages()
