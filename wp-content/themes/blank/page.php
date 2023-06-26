0d3c1eac0dc475f0d0884815991a11fbdee8acfc
David Vielhuber <david@vielhuber.de>
2023-06-26 16:20:39 +0200

    Removejs and css.

--- wp-content/themes/blank/page.php
+++ wp-content/themes/blank/page.php
@@ -5,6 +5,16 @@ get_header();
 
     <main id="main">
         <?php
+        // wordpress style
+        /*
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+        }
+        */
+
         // breadcrumbs
         if (!is_front_page()) {
             include(get_template_directory() . '/_templates/_common/breadcrumbs.php');
