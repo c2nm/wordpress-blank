633322168601512009a8e7ec370c97f9f7b24f9c
David Vielhuber <david@vielhuber.de>
2024-01-04 02:56:58 +0100

    Add comment.

--- wp-content/themes/blank/_php/Core.php
+++ wp-content/themes/blank/_php/Core.php
@@ -954,6 +954,7 @@ $rand
 
     private function disableWpCors()
     {
+        /* disable wordpress native cors settings (so that .htaccess settings are applied) */
         add_action(
             'rest_api_init',
             function () {
